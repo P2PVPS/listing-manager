5092643680bbf8e91381b9f33499b86411e0fd59
Chris Troutner <chris.troutner@gmail.com>
2018-09-07 18:14:21 -0700

    Got mocha working

---
+++ test/temp.js
@@ -0,0 +1,17 @@
+/*
+*/
+
+"use strict";
+
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("Temp", () => {
+  describe("Second level", () => {
+    it("this is a test", async () => {
+      console.log("hello world!");
+
+      assert(true, true, "message");
+    });
+  });
+});
